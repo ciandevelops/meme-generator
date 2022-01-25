99c1b85cfb9b338bfdfef8dfa2665a3b469cc353
Cian Butler <ciandevelops@gmail.com>
2022-01-25 12:07:15 -0500

    fonts added and header layout

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <img src="/images/troll.png" alt="logo" />
+      <h1>Meme Generator</h1>
+
+      <h3>React Course - Project 3</h3>
+    </nav>
+  );
+};
+
+export default Header;
