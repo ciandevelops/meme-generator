10c4d7737f2d0ea559056c6a1996bd5d05985144
Cian Butler <ciandevelops@gmail.com>
2022-01-25 12:34:18 -0500

    navbar complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,14 @@ import React from "react";
 const Header = () => {
   return (
     <nav>
-      <img src="/images/troll.png" alt="logo" />
-      <h1>Meme Generator</h1>
+      <div className="nav--left">
+        <img src="/images/troll.png" alt="logo" />
+        <h1 className="nav--title">Meme Generator</h1>
+      </div>
 
-      <h3>React Course - Project 3</h3>
+      <div className="nav--right">
+        <h3 className="nav--subtitle">React Course - Project 3</h3>
+      </div>
     </nav>
   );
 };
