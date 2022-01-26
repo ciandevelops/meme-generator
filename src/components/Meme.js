756841805a0662921af16df3b9bc4d1224ad5db9
Cian Butler <ciandevelops@gmail.com>
2022-01-25 21:30:50 -0500

    state added to get memes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData";
 
 const Meme = () => {
+  const [memeImage, setMemeImage] = useState("");
+
+  const getMemeImage = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMemeImage(url);
+  };
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top Text" className="meme--inputs" />
         <input type="text" placeholder="Bottom Text" className="meme--inputs" />
 
-        <button className="meme--button">Get a new meme image 🖼</button>
-      </form>
+        <button onClick={getMemeImage} className="meme--button">
+          Get a new meme image 🖼
+        </button>
+      </div>
+
+      <img src={memeImage} alt="meme" />
     </main>
   );
 };
